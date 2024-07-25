ac18d996c3bce476b78c3796652bccf6228280ab
Takamine Kota <kota@TakaminenoMacBook-Air.local>
2024-07-25 15:25:13 +0900

    動的追加

--- Stulab2/app.py
+++ Stulab2/app.py
@@ -7,11 +7,11 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/register')
+@app.route('/new_income')#そのhtmlに飛ぶコード
 def register():
     return render_template('register.html')
 
-@app.route('/submit_registration', methods=['POST'])
+@app.route('/submit_registration', methods=['POST'])#フォームを出すコード
 def submit_registration():
     # フォームからのデータを取得
     username = request.form['username']
