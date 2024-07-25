20225f2a2ecd687e21d3b6127e543446917d3efa
Takamine Kota <kota@TakaminenoMacBook-Air.local>
2024-07-25 14:56:25 +0900

    目次とボタンの追加

--- Stulab2/app.py
+++ Stulab2/app.py
@@ -1,1 +1,23 @@
+from flask import Flask, render_template,request
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/submit_registration', methods=['POST'])
+def submit_registration():
+    # フォームからのデータを取得
+    username = request.form['username']
+    password = request.form['password']
+    email = request.form['email']
+    return f"Registration Received: Username - {username}, Email - {email}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
